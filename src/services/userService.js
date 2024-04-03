de8894e709f38338384627263273a492892f3139
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-04-03 14:24:15 -0500

    Add login wireframe and sample user service

---
+++ src/services/userService.js
@@ -0,0 +1,17 @@
+export const getEmployeeByEmail = (email) => {
+    const sampleEmployee = {
+        id: 1,
+        address: "Pizza town",
+        phone: "5554344343",
+        email: "giuseppi@isthebesti.com",
+        admin: true
+
+    }
+    return sampleEmployee;
+    
+    // fetch(`http://localhost:8088/employees?email=${email}`).then((res) =>
+    //   res.json()
+    // )
+
+
+  }
